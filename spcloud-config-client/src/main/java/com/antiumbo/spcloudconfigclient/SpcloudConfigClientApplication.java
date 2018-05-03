31050dd6905bbf6a0cdd355e70b76a8ed14d2d40
Bunnos <antiumbo@icloud.com>
2018-05-03 15:09:08 +0800

    config修改更新

--- spcloud-config-client/src/main/java/com/antiumbo/spcloudconfigclient/SpcloudConfigClientApplication.java
+++ spcloud-config-client/src/main/java/com/antiumbo/spcloudconfigclient/SpcloudConfigClientApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpcloudConfigClientApplication implements EmbeddedServletContainerCustomizer {
 
