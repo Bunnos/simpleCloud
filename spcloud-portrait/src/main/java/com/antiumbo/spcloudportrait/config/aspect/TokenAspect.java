ac989a99b588b57deff83976a935950fd17012de
ANTIUMBO\antiumbo <boyang.li@jsxfedu.com>
2018-12-21 17:38:35 +0800

    优化

--- spcloud-portrait/src/main/java/com/antiumbo/spcloudportrait/config/aspect/TokenAspect.java
+++ spcloud-portrait/src/main/java/com/antiumbo/spcloudportrait/config/aspect/TokenAspect.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
